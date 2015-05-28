3e566346b08294b9fea2be1ef5be19fadfa03d7c
mavanes <meroavanessian@gmail.com>
2015-05-28 08:28:04 -0700

    Update DataAcquisition.py

--- DataAcquisition.py
+++ DataAcquisition.py
@@ -10,11 +10,40 @@ def convert_to_datetime(timestamp):
 def format_date(date):
     return date.replace('T', ' ').replace('Z', '')
 
+def read_dailymotion(max_pages):
+    limit = 1
+    results = []
+    url = "https://api.dailymotion.com/videos?fields=id,created_time,title,views_total,duration,description,&sort=visited&limit=100&page="
+    r = requests.get(url + str(limit))
+    struc = r.json()
+    results.append(struc)
+    while struc[u'has_more'] and limit < max_pages:
+        limit += 1
+        r = requests.get(url + str(limit))
+        struc = r.json()
+        results.append(struc)
+    return results
+    
+def read_youtube(max_pages):
+    limit = 0
+    results = []
+    url = "https://www.googleapis.com/youtube/v3/videos?part=id%2Cstatistics%2Csnippet%2CcontentDetails&chart=mostPopular&max-results=50&key=AIzaSyDMg-eb-hHji1WEF_H_je1SXSt9HsMeofU"
+    r = requests.get(url)
+    struc = r.json()
+    results.append(struc)
+    while u'nextPageToken' in struc and limit < max_pages:
+        print (url + '&pageToken=' + str(struc['nextPageToken']))
+        r = requests.get(url + '&pageToken=' + str(struc['nextPageToken']))
+        struc = r.json()
+        results.append(struc)
+        limit+=1
+    return results
+    
 def main():
     f = open('data.txt', 'w')
     limit = 1
     url = "https://api.dailymotion.com/videos?fields=created_time,title,views_total,&sort=visited&limit=100&page="
-    r = requests.get(url + str(limit))
+    r = requests.get(url + str(limit)) 
     struc = r.json()
     for x in struc[u'list']:
         f.write(x[u'title'].encode('utf-8') + '\n')
@@ -43,6 +72,7 @@ def main():
         f.write('created time: ' + str(format_date(x[u'snippet'][u'publishedAt'])) + '\n\n')
         
     while u'nextPageToken' in struc and limit < 10:
+        print url + '&pageToken=' + struc[u'nextPageToken']
         r = requests.get(url + '&pageToken=' + struc[u'nextPageToken'])
         struc = r.json()
         for x in struc[u'items']:
